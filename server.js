1828a94aabd55fd76c951b22d98ae92877caa4ae
Limbureuben <limbureuben4@gmail.com>
2025-05-17 21:32:41 +0300

    changes

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ const startServer = async () => {
 
   const PORT = process.env.PORT || 4000;
   app.listen(PORT, () => {
-    console.log(`🚀 Server ready at http://localhost:${PORT}${server.graphqlPath}`);
+    console.log(`🚀 Dear Reuben Server ready at http://localhost:${PORT}${server.graphqlPath}`);
   });
 };
 

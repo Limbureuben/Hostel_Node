407c57b0a38b73c28b1455169632849bafb8b4f1
Limbureuben <limbureuben4@gmail.com>
2025-05-17 16:51:10 +0300

    changes

---
+++ src/schema/CustomerUser.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const { modelName } = require('../models/User');
+
+const customerUserSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    passwordConfirm: { type: String, required: true },
+},
+ { timestamps: true }
+);
+
+module.exports = mongoose.model('CustomerUser', customerUserSchema );

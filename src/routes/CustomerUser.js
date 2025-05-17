17d73b85abb1d05ef95bf6aabbd3b9068b0a4992
Limbureuben <limbureuben4@gmail.com>
2025-05-17 17:17:16 +0300

    changes

---
+++ src/routes/CustomerUser.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const CustomerUser = require('../models/CustomerUser');
+require('dotenv').config();
+
+const router = express.Router();
+
+router.post('/signup', async (req, res) => {
+    const { name, email, password, confirmPassword } = req.body;
+
+    try {
+        const existing = await CustomerUser.findOne({ email });
+        if (existing) return res.status(400).json({ message: 'Email already in use' });
+
+        const MatchPassword = password === confirmPassword;
+        if (!MatchPassword) return res.status(400).json({ message: 'Passwords do not match' });
+
+        const hashedPassword = await bcrypt.hash(password, 12);
+
+        const newUser = await CustomerUser.create({ name, email, password: hashedPassword, passwordConfirm: confirmPassword });
+        
+        const token = jwt.sign({ id: newUser._id, email: newUser.email }, process.env.JWT_SECRET, { expiresIn: '1h'});
+
+        res.status(201).json({ user: { id: newUser._id, name: newUser.name, email: newUser.email }, token});
+    
+    } catch (error) {
+        res.status(500).json({ message: 'Registration failed', error: error.message });
+    }
+});

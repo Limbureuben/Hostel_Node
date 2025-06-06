3b7918f2da4d1fa53809e6781c5ebfe8256b2ca6
Limbureuben <limbureuben4@gmail.com>
2025-06-06 21:40:29 +0300

    changes

--- src/models/Organization.js
+++ src/models/Organization.js
@@ -4,14 +4,19 @@ const OrganizationSchema = new mongoose.Schema({
     organization_name: { type: String, required: true },
     discriptions: { type: String, required: true },
     contact: { type: String, required: true },
-    location: { type: String, required: true }
+    location: { type: String, required: true },
+    logo: { type: String },
+    created_by: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }
+
 })
 
 const OrganizationPackageSchema = new mongoose.Schema({
     package_name: { type: String, required: true},
     discriptions: { type: String, required: true },
     link: { type: String, required: true },
-    price: { type: String, required: true }
+    logo: { type: String },
+    price: { type: String, required: true },
+    created_by: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }
 })
 
 const Organization = mongoose.model('Organization', OrganizationSchema);

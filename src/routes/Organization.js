2567266f9da109a9316232bec2108563497f8399
Limbureuben <limbureuben4@gmail.com>
2025-06-06 12:29:23 +0300

    changes

--- src/routes/Organization.js
+++ src/routes/Organization.js
@@ -1,32 +1,37 @@
 const express = require('express');
-
 const { Organization, Packages } = '../models/Organization.js'
 require('dotenv').config();
 
 const router = express.Router();
 
-router.post('/registerorganization', async(req, res) => {
+router.post('/registerorganization', async (req, res) => {
     const { organization_name, discriptions, contact, location } = req.body;
 
-   try {
-     const newOrganization = await Organization.create({
-        organization_name,
-        discriptions,
-        contact,
-        location
-    });
-    res.status(201).json({ organization: { id: newOrganization.organization_id, name: newOrganization.organization_name, location: newOrganization.location} });
-   } catch (error) {
-    res.status(500).json({
-        message: 'Organization registration failed',
-        error: error.message
-    })
-   }
-})
+    try {
+        const newOrganization = await Organization.create({
+            organization_name,
+            discriptions,
+            contact,
+            location
+        });
 
+        res.status(201).json({
+            organization: {
+                id: newOrganization._id,
+                name: newOrganization.organization_name,
+                location: newOrganization.location
+            }
+        });
+    } catch (error) {
+        res.status(500).json({
+            message: 'Organization registration failed',
+            error: error.message
+        });
+    }
+});
 
 router.post('/registerpackage', async (req, res) => {
-    const { package_name, discriptions, link, price } = res.body;
+    const { package_name, discriptions, link, price } = req.body;
 
     try {
         const newPackage = await Packages.create({
@@ -34,21 +39,21 @@ router.post('/registerpackage', async (req, res) => {
             discriptions,
             link,
             price
-        })
+        });
+
         res.status(201).json({
-            Packages: {
-                id: newPackage.package_id,
+            package: {
+                id: newPackage._id,
                 name: newPackage.package_name,
                 link: newPackage.link,
                 price: newPackage.price
             }
-        })
-    } catch(error) {
+        });
+    } catch (error) {
         res.status(500).json({
-            message: 'Failed to register the packages',
+            message: 'Failed to register the package',
             error: error.message
-        })
+        });
     }
-})
-
+});
 module.exports = router;

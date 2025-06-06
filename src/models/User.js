ee0c9bec6b30c8cc4b0fbe44a8e16dafad854647
Limbureuben <limbureuben4@gmail.com>
2025-06-07 00:12:21 +0300

    changes

--- src/models/User.js
+++ src/models/User.js
@@ -5,6 +5,11 @@ const userSchema = new mongoose.Schema({
   username: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  role: { 
+    type: String,
+    enum: ['admin', 'staff', 'user'],
+    default: 'user'
+   }
 });
 
 userSchema.pre('save', async function(next) {

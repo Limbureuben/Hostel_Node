e2a8f09fcfb2cb073b54c0cd008b0ea1bbcb69c5
Limbureuben <limbureuben4@gmail.com>
2025-06-06 22:34:06 +0300

    changes

--- server.js
+++ server.js
@@ -1,39 +1,95 @@
 const express = require('express');
-const { ApolloServer } = require('apollo-server-express');
-const typeDefs = require('./src/schema/userSchema');
-const resolvers = require('./src/resolvers/userResolvers');
 const connectDB = require('./src/config/db');
 const CustomerRouter = require('./src/routes/CustomerUser');
 const StudentRouter = require('./src/routes/Student');
-const OrganizationRouter = require('./src/routes/Organization')
-const PackagesRouter = require('./src/routes/Organization')
+const OrganizationRouter = require('./src/routes/Organization');
+const PackagesRouter = require('./src/routes/Organization'); // If both are same, remove duplication
 
 const startServer = async () => {
   const app = express();
-
   app.use(express.json());
 
   await connectDB();
 
+  // REST API routes
   app.use('/api/customer', CustomerRouter);
-  app.use('/api/registerStudent', StudentRouter );
-  app.use('/api/addorganization', OrganizationRouter);
-  app.use('/api/registerPackages', PackagesRouter);
-  app.use('/api/getOrganizations', OrganizationRouter);
-  app.use('/api/getPackages', PackagesRouter)
-
-  const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-  });
-
-  await server.start();
-  server.applyMiddleware({ app });
+  app.use('/api/registerStudent', StudentRouter);
+  app.use('/api/organization', OrganizationRouter);
+  app.use('/api/packages', PackagesRouter);
 
   const PORT = process.env.PORT || 4000;
   app.listen(PORT, () => {
-    console.log(`🚀 Dear Reuben Server ready at http://localhost:${PORT}${server.graphqlPath}`);
+    console.log(`Server ready at http://localhost:${PORT}`);
   });
 };
 
 startServer();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const express = require('express');
+// const { ApolloServer } = require('apollo-server-express');
+// const typeDefs = require('./src/schema/userSchema');
+// const resolvers = require('./src/resolvers/userResolvers');
+// const connectDB = require('./src/config/db');
+// const CustomerRouter = require('./src/routes/CustomerUser');
+// const StudentRouter = require('./src/routes/Student');
+// const OrganizationRouter = require('./src/routes/Organization')
+// const PackagesRouter = require('./src/routes/Organization')
+
+// const startServer = async () => {
+//   const app = express();
+
+//   app.use(express.json());
+
+//   await connectDB();
+
+//   app.use('/api/customer', CustomerRouter);
+//   app.use('/api/registerStudent', StudentRouter );
+//   app.use('/api/addorganization', OrganizationRouter);
+//   app.use('/api/registerPackages', PackagesRouter);
+//   app.use('/api/getOrganizations', OrganizationRouter);
+//   app.use('/api/getPackages', PackagesRouter)
+
+//   const server = new ApolloServer({
+//     typeDefs,
+//     resolvers,
+//   });
+
+//   await server.start();
+//   server.applyMiddleware({ app });
+
+//   const PORT = process.env.PORT || 4000;
+//   app.listen(PORT, () => {
+//     console.log(`🚀 Dear Reuben Server ready at http://localhost:${PORT}${server.graphqlPath}`);
+//   });
+// };
+
+// startServer();

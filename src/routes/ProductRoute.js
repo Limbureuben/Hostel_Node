56e337deb4e31f79c74f15fbfbfdc30fb3e682db
Limbureuben <limbureuben4@gmail.com>
2025-07-05 21:09:14 +0300

    changes

--- src/routes/ProductRoute.js
+++ src/routes/ProductRoute.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const { Product } = require('../models/Product');
 require('dotenv').config();
+const authMiddleware = require('../utils/authMiddleware');
 
 const router = express.Router()
 
-router.post('/register-product', async (req, res) => {
+router.post('/register-product', authMiddleware, async (req, res) => {
     const { product_name, product_descriptions, product_category, discount_price, product_price, product_image } = req.body;
 
     try {
